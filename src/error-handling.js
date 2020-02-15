05d7048eaf381f6596ae30e9b064ef3f3b4c3b07
Michele Triaca <michele.triaca@telnext.com>
2020-02-15 14:26:54 +0100

    [errorhandling] red errors

--- src/error-handling.js
+++ src/error-handling.js
@@ -1,5 +1,7 @@
+const chalk = require('chalk')
+
 const handleError = e => {
-  console.error(typeof (e) === 'string' ? e : e.message)
+  console.error(chalk.red(typeof (e) === 'string' ? e : e.message))
   process.exit(1)
 }
 
