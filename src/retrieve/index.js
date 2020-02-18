2aa72e0eb203673eda0249332604c223cbfa520e
Michele Triaca <michele.triaca@telnext.com>
2020-02-18 18:24:00 +0100

    [libraryMode] retrieve

---
+++ src/retrieve/index.js
@@ -0,0 +1,85 @@
+
+const chalk = require('chalk')
+const log = require('../services/log-service').getLogger()
+const decompress = require('decompress')
+const Sfdc = require('../utils/sfdc-utils')
+const stripEmptyTranslations = require('../prepare/strip-empty-translations')
+const stripUselessFlsInPermissionSets = require('../prepare/strip-useless-fls-in-permission-sets')
+const stripPartnerRoles = require('../prepare/strip-partner-roles')
+const fixProfiles = require('../prepare/fix-profiles')
+const { getMembersOf, getProfileOnlyPackage, getPackageXml } = require('../utils/package-utils')
+const { printLogo } = require('../utils/branding-utils')
+const multimatch = require('multimatch')
+const pluginEngine = require('../plugin-engine')
+const path = require('path')
+const pathService = require('../services/path-service')
+const fs = require('fs')
+
+module.exports = async ({ loginOpts, basePath, logger, profileOnly, files, meta, config }) => {
+  console.time('running time')
+  printLogo()
+  log(chalk.yellow(`(1/4) Logging in salesforce as ${loginOpts.username}...`))
+  const sfdcConnector = await Sfdc.newInstance({
+    username: loginOpts.username,
+    password: loginOpts.password,
+    isSandbox: !!loginOpts.sandbox,
+    serverUrl: loginOpts.serverUrl
+  })
+  log(chalk.green(`Logged in!`))
+
+  await pluginEngine.registerPlugins(config.postRetrievePlugins, sfdcConnector, loginOpts.username)
+
+  log(chalk.yellow(`(2/4) Retrieving metadata...`))
+  if (profileOnly) log(chalk.yellow(`--profile-only=true. Retrieving profiles only...`))
+  const specificFiles = (files && files.split(',').map(x => x.trim())) || []
+  const specificMeta = (meta && meta.split(',').map(x => x.trim())) || []
+  if (specificFiles.length) log(chalk.yellow(`--files specified. Retrieving only specific files...`))
+  else if (specificMeta.length) log(chalk.yellow(`--meta specified. Retrieving only specific metadata types...`))
+  const pkgJson = await (
+    profileOnly
+      ? getProfileOnlyPackage()
+      : getPackageXml({
+        specificFiles,
+        specificMeta,
+        sfdcConnector
+      })
+  )
+  if (specificFiles.length) log(chalk.yellow(`delta package generated`))
+
+  const retrieveJob = await sfdcConnector.retrieveMetadata(pkgJson, config.profiles && config.profiles.addExtraApplications)
+  const retrieveResult = await sfdcConnector.pollRetrieveMetadataStatus(retrieveJob.id)
+  log(chalk.green(`Retrieve completed!`))
+  log(chalk.yellow(`(3/4) Unzipping...`))
+  const zipBuffer = Buffer.from(retrieveResult.zipFile, 'base64')
+  await decompress(zipBuffer, path.resolve(pathService.getBasePath(), 'src'), {
+    filter: f => profileOnly ? f.path.endsWith('.profile') : !/package\.xml$/.test(f.path)
+  })
+  log(chalk.green(`Unzipped!`))
+  log(chalk.yellow(`(4/4) Applying patches...`))
+  const patchProfiles = pkgJson.types.some(x => x.name[0] === 'Profile')
+  const patchTranslations = pkgJson.types.some(x => x.name[0] === 'CustomObjectTranslation')
+  const patchPermissionSet = pkgJson.types.some(x => x.name[0] === 'PermissionSet')
+  const patchPartnerRoles = pkgJson.types.some(x => x.name[0] === 'Role')
+
+  await Promise.all([
+    patchTranslations ? stripEmptyTranslations(config) : Promise.resolve(),
+    patchPermissionSet ? stripUselessFlsInPermissionSets(config) : Promise.resolve(),
+    patchProfiles ? fixProfiles(config, sfdcConnector) : Promise.resolve(),
+    patchPartnerRoles ? Promise.resolve(stripPartnerRoles(config)) : Promise.resolve()
+  ])
+
+  await pluginEngine.applyTransformationsAndWriteBack(specificFiles, sfdcConnector)
+
+  const APPS_PATH = path.resolve(pathService.getBasePath(), 'src', 'applications')
+  if (fs.existsSync(APPS_PATH) && pkgJson.types.some(x => x.name[0] === 'Profile')) {
+    const versionedApps = (await getMembersOf('CustomApplication')).map(x => x + '.app')
+    if (!versionedApps.length) fs.removeSync(APPS_PATH)
+    else {
+      fs.readdirSync(APPS_PATH).map(f => {
+        if (!multimatch(f, versionedApps).length) fs.unlinkSync(path.resolve(APPS_PATH, f))
+      })
+    }
+  }
+  log(chalk.green(`Patches applied!`))
+  console.timeEnd('running time')
+}
--- src/sfdy-retrieve.js
+++ src/sfdy-retrieve.js
@@ -1,21 +1,10 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-const chalk = require('chalk')
-const log = require('./services/log-service').getLogger()
 const fs = require('fs-extra')
 const path = require('path')
-const decompress = require('decompress')
-const Sfdc = require('./utils/sfdc-utils')
-const stripEmptyTranslations = require('./prepare/strip-empty-translations')
-const stripUselessFlsInPermissionSets = require('./prepare/strip-useless-fls-in-permission-sets')
 const pathService = require('./services/path-service')
-const stripPartnerRoles = require('./prepare/strip-partner-roles')
-const fixProfiles = require('./prepare/fix-profiles')
-const { getMembersOf, getProfileOnlyPackage, getPackageXml } = require('./utils/package-utils')
-const { printLogo } = require('./utils/branding-utils')
-const multimatch = require('multimatch')
-const pluginEngine = require('./plugin-engine')
+const retrieve = require('./retrieve')
 require('./error-handling')()
 
 program
@@ -36,70 +25,11 @@ if (!fs.existsSync(configPath)) throw Error('Missing configuration file .sfdy.js
 
 const config = require(configPath)
 
-;(async () => {
-  console.time('running time')
-  printLogo()
-  log(chalk.yellow(`(1/4) Logging in salesforce as ${program.username}...`))
-  const sfdcConnector = await Sfdc.newInstance({
-    username: program.username,
-    password: program.password,
-    isSandbox: !!program.sandbox
-  })
-  log(chalk.green(`Logged in!`))
-
-  await pluginEngine.registerPlugins(config.postRetrievePlugins, sfdcConnector, program.username)
-
-  log(chalk.yellow(`(2/4) Retrieving metadata...`))
-  if (program.profileOnly) log(chalk.yellow(`--profile-only=true. Retrieving profiles only...`))
-  const specificFiles = (program.files && program.files.split(',').map(x => x.trim())) || []
-  const specificMeta = (program.meta && program.meta.split(',').map(x => x.trim())) || []
-  if (specificFiles.length) log(chalk.yellow(`--files specified. Retrieving only specific files...`))
-  else if (specificMeta.length) log(chalk.yellow(`--meta specified. Retrieving only specific metadata types...`))
-  const pkgJson = await (
-    program.profileOnly
-      ? getProfileOnlyPackage()
-      : getPackageXml({
-        specificFiles,
-        specificMeta,
-        sfdcConnector
-      })
-  )
-  if (specificFiles.length) log(chalk.yellow(`delta package generated`))
-
-  const retrieveJob = await sfdcConnector.retrieveMetadata(pkgJson, config.profiles && config.profiles.addExtraApplications)
-  const retrieveResult = await sfdcConnector.pollRetrieveMetadataStatus(retrieveJob.id)
-  log(chalk.green(`Retrieve completed!`))
-  log(chalk.yellow(`(3/4) Unzipping...`))
-  const zipBuffer = Buffer.from(retrieveResult.zipFile, 'base64')
-  await decompress(zipBuffer, path.resolve(pathService.getBasePath(), 'src'), {
-    filter: f => program.profileOnly ? f.path.endsWith('.profile') : !/package\.xml$/.test(f.path)
-  })
-  log(chalk.green(`Unzipped!`))
-  log(chalk.yellow(`(4/4) Applying patches...`))
-  const patchProfiles = pkgJson.types.some(x => x.name[0] === 'Profile')
-  const patchTranslations = pkgJson.types.some(x => x.name[0] === 'CustomObjectTranslation')
-  const patchPermissionSet = pkgJson.types.some(x => x.name[0] === 'PermissionSet')
-  const patchPartnerRoles = pkgJson.types.some(x => x.name[0] === 'Role')
-
-  await Promise.all([
-    patchTranslations ? stripEmptyTranslations(config) : Promise.resolve(),
-    patchPermissionSet ? stripUselessFlsInPermissionSets(config) : Promise.resolve(),
-    patchProfiles ? fixProfiles(config, sfdcConnector) : Promise.resolve(),
-    patchPartnerRoles ? Promise.resolve(stripPartnerRoles(config)) : Promise.resolve()
-  ])
-
-  await pluginEngine.applyTransformationsAndWriteBack(specificFiles, sfdcConnector)
-
-  const APPS_PATH = path.resolve(pathService.getBasePath(), 'src', 'applications')
-  if (fs.existsSync(APPS_PATH) && pkgJson.types.some(x => x.name[0] === 'Profile')) {
-    const versionedApps = (await getMembersOf('CustomApplication')).map(x => x + '.app')
-    if (!versionedApps.length) fs.removeSync(APPS_PATH)
-    else {
-      fs.readdirSync(APPS_PATH).map(f => {
-        if (!multimatch(f, versionedApps).length) fs.unlinkSync(path.resolve(APPS_PATH, f))
-      })
-    }
-  }
-  log(chalk.green(`Patches applied!`))
-  console.timeEnd('running time')
-})()
+retrieve({
+  basePath: pathService.getBasePath(),
+  config,
+  files: program.files,
+  loginOpts: program,
+  meta: program.meta,
+  profileOnly: program.profileOnly
+})
