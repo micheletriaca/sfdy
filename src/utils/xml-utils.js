c2edda16af665be5d6faf5bd1e45a9cf20680157
Michele Triaca <michele.triaca@telnext.com>
2020-03-01 12:28:24 +0100

    [xml] custom xml builder

--- src/utils/xml-utils.js
+++ src/utils/xml-utils.js
@@ -2,20 +2,75 @@ const util = require('util')
 const xml2js = require('xml2js')
 
 const parser = util.promisify(new xml2js.Parser({ explicitArray: true }).parseString)
-const builder = new xml2js.Builder({
-  renderOpts: {
-    'pretty': true,
-    'indent': '    ',
-    'newline': '\n'
-  },
-  xmldec: {
-    standalone: undefined,
-    encoding: 'UTF-8',
-    version: '1.0'
+
+const renderOpts = {
+  indent: '    ',
+  emptyTag: true
+}
+
+const serializeValue = (tagname, v, indentLevel) => {
+  let res = ''
+  if (typeof (v) === 'object') {
+    res += '\n'
+    const keys = Object.keys(v)
+    const l = keys.length
+    for (let i = 0; i < l; i++) {
+      const serializedSubXml = sfdcXmlBuilder(keys[i], v[keys[i]], false, indentLevel)
+      res += serializedSubXml + (serializedSubXml ? '\n' : '')
+    }
+  } else {
+    v = v + ''
+    const l = v.length
+    for (let i = 0; i < l; i++) {
+      const c = v[i]
+      if (c === '&') {
+        res += '&amp;'
+      } else if (c === '<') {
+        res += '&lt;'
+      } else if (c === '>') {
+        res += '&gt;'
+      } else if (c === '"') {
+        res += '&quot;'
+      } else if (c === "'") {
+        res += '&apos;'
+      } else {
+        res += c
+      }
+    }
   }
-})
+  return res
+}
+
+const buildTag = (tagname, attributes) => {
+  let res = '<' + tagname
+  const attrs = Object.keys(attributes).map(k => `${k}="${attributes[k]}"`).join(' ')
+  if (attrs) res += ' ' + attrs
+  res += '>'
+  return res
+}
+
+/**
+ *  We need a custom xml serializer because salesfore escapes perfectly valid chars in xml (such as ' or ")
+ *  and xml2js does not handle this weirdness
+ */
+const sfdcXmlBuilder = (tagname, value, isRoot = true, indentLevel = '') => {
+  let res = isRoot ? '<?xml version="1.0" encoding="UTF-8"?>\n' : ''
+  if (!Array.isArray(value)) value = [value]
+  const l = value.length
+  for (let i = 0; i < l; i++) {
+    const isObject = typeof (value[i]) === 'object' && !value[i].hasOwnProperty('_')
+    res += indentLevel + buildTag(tagname, value[i].$ || {})
+    delete value[i].$
+    const serializedValue = serializeValue(tagname, value[i]._ || value[i], indentLevel + renderOpts.indent)
+    res += serializedValue
+    if (!serializedValue && renderOpts.emptyTag) res = res.replace(/>$/, '/>')
+    else res += (isObject ? indentLevel : '') + '</' + tagname + '>'
+    if (i < value.length - 1) res += '\n'
+  }
+  return res
+}
 
 module.exports = {
   parseXml: parser,
-  buildXml: builder.buildObject.bind(builder)
+  buildXml: v => sfdcXmlBuilder(Object.keys(v)[0], Object.values(v)[0])
 }
