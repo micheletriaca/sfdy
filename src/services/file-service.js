9e54a115a0b59efefe6f308a77b70576f5f826c6
Michele Triaca <michele.triaca@telnext.com>
2022-02-19 21:23:15 +0100

    [refactoring] removed deprecated code

--- src/services/file-service.js
+++ src/services/file-service.js
@@ -11,20 +11,5 @@ module.exports = {
         fileName: f,
         data: fs.readFileSync(path.join(rootFolder, f))
       }))
-  },
-  readAllFilesInFolder (rootFolder, folder) {
-    const realPath = path.join(rootFolder, folder || '')
-    return fs.readdirSync(realPath).flatMap(file => {
-      const fRelativePath = path.join(folder || '', file)
-      const fAbsolutePath = path.join(rootFolder, fRelativePath)
-      if (fs.lstatSync(fAbsolutePath).isDirectory()) {
-        return module.exports.readAllFilesInFolder(rootFolder, fRelativePath)
-      } else {
-        return {
-          fileName: fRelativePath,
-          data: fs.readFileSync(fAbsolutePath)
-        }
-      }
-    })
   }
 }
