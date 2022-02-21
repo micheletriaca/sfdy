519ff5d7e4fc2bfa5ab7dffa373aa8595b5b7a8b
Michele Triaca <michele.triaca@telnext.com>
2022-02-21 14:14:09 +0100

    [megarefatoring] fix strip manage package plugin

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,14 +1,14 @@
 module.exports = [
 /*  require('./dependency-graph'),
+require('./strip-fls-in-permission-sets'),
+require('./profile-plugins/strip-immutable-stuff-from-std-profiles'),
+require('./profile-plugins/add-all-permissions-to-custom-profiles'),
+require('./profile-plugins/add-objects-to-profiles'),
+require('./profile-plugins/add-tab-visibilities-to-profiles'),
+require('./profile-plugins/add-application-visibilities-to-profiles'),
+require('./strip-unversioned-stuff')
+*/
   require('./strip-managed-package-stuff'),
-  require('./strip-fls-in-permission-sets'),
-  require('./profile-plugins/strip-immutable-stuff-from-std-profiles'),
-  require('./profile-plugins/add-all-permissions-to-custom-profiles'),
-  require('./profile-plugins/add-objects-to-profiles'),
-  require('./profile-plugins/add-tab-visibilities-to-profiles'),
-  require('./profile-plugins/add-application-visibilities-to-profiles'),
-  require('./strip-unversioned-stuff')
-  */
   require('./strip-translations'),
   require('./strip-partner-roles')
 ]
--- src/plugins/strip-managed-package-stuff.js
+++ src/plugins/strip-managed-package-stuff.js
@@ -1,9 +1,7 @@
-module.exports = async (context, helpers) => {
-  if (!context.config.stripManagedPackageFields) return
-
-  const namespaces = context.config.stripManagedPackageFields
+const afterRetrieve = async (namespaces, xmlTransformer) => {
   const fn = apiName => x => !namespaces.some(mp => x[apiName][0].startsWith(mp))
-  helpers.xmlTransformer('objects/**/*', async (filename, fJson) => {
+
+  await xmlTransformer('objects/**/*', async (filename, fJson) => {
     fJson.fields = (fJson.fields || []).filter(fn('fullName'))
     fJson.webLinks = (fJson.webLinks || []).filter(fn('fullName'))
     ;(fJson.recordTypes || [])
@@ -11,11 +9,14 @@ module.exports = async (context, helpers) => {
       .forEach(v => (v.picklistValues = v.picklistValues.filter(fn('picklist'))))
   })
 
-  helpers.xmlTransformer('permissionsets/**/*', async (filename, fJson) => {
+  await xmlTransformer(['permissionsets/**/*', 'profiles/**/*'], async (filename, fJson) => {
     fJson.fieldPermissions = (fJson.fieldPermissions || []).filter(fn('field'))
   })
+}
 
-  helpers.xmlTransformer('profiles/**/*', async (filename, fJson) => {
-    fJson.fieldPermissions = (fJson.fieldPermissions || []).filter(fn('field'))
-  })
+module.exports = {
+  afterRetrieve: async (ctx, { xmlTransformer }) => {
+    if (!ctx.config.stripManagedPackageFields) return
+    await afterRetrieve(ctx.config.stripManagedPackageFields, xmlTransformer)
+  }
 }
