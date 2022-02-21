7bc4fed7d0d329e95bd0902ab4710cab4d970da3
Michele Triaca <michele.triaca@telnext.com>
2022-02-21 14:19:32 +0100

    [megarefatoring] fix strip immutable stuff from std profiles

--- src/plugins/profile-plugins/strip-immutable-stuff-from-std-profiles.js
+++ src/plugins/profile-plugins/strip-immutable-stuff-from-std-profiles.js
@@ -1,12 +1,14 @@
-const get = require('lodash').get
+const _ = require('exstream.js')
+const isPluginEnabled = _.makeGetter('config.profiles.stripUserPermissionsFromStandardProfiles', false)
 
-module.exports = async (context, helpers) => {
-  if (!get(context, 'config.profiles.stripUserPermissionsFromStandardProfiles')) return
-
-  helpers.xmlTransformer('profiles/**/*', async (filename, fJson) => {
-    if (fJson.custom && fJson.custom[0] !== 'true') {
-      fJson.userPermissions = []
-      fJson.objectPermissions = []
-    }
-  })
+module.exports = {
+  afterRetrieve: async (ctx, { xmlTransformer }) => {
+    if (!isPluginEnabled(ctx)) return
+    await xmlTransformer('profiles/**/*', async (filename, fJson) => {
+      if (fJson.custom && fJson.custom[0] !== 'true') {
+        fJson.userPermissions = []
+        fJson.objectPermissions = []
+      }
+    })
+  }
 }
